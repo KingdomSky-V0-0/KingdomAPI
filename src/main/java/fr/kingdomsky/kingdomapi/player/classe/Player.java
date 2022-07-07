8609aa885720b1535b2c207570ef3ef1e1417ae1
MegaCretin <megacretin@gmail.com>
2022-07-07 23:56:26 +0200

    Ajout classe Player.java

---
+++ src/main/java/fr/kingdomsky/kingdomapi/player/classe/Player.java
@@ -0,0 +1,18 @@
+package fr.kingdomsky.kingdomapi.player.classe;
+
+public class Player {
+
+    private Player player;
+
+    public Player(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
